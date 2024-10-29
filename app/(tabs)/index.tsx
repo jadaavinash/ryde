bb4f50782b66804f854e10addc4e4a944a494ec4
jadaavinash <jada.avinash2021@vitstudent.ac.in>
2024-10-29 17:08:30 +0530

    All setup done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
-import {View, Text, StatusBar } from 'react-native';
+import { Text, StatusBar } from "react-native";
+import SafeAreaView from "react-native-safe-area-view";
+
 export default function HomeScreen() {
   return (
-      <View className="flex-1 items-center justify-center bg-white">
-          <Text>Open up App.js to start working on your app!</Text>
-          <StatusBar style="auto" />
-      </View>
-
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className={"text-red-500"}>Ryde Clone</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,6 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-import { Slot } from "expo-router";
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
